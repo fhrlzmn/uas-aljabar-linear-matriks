15a6af540a1f1e257b629dc658c4a3d361ee9328
fhrlzmn <fhrlzmn26@gmail.com>
2022-01-05 12:42:46 +0700

    Responsive navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { Popover, Transition } from '@headlessui/react';
 
 const navMenu = [
   ['/contact', 'Contact'],
@@ -8,28 +9,33 @@ const navMenu = [
 
 export default function Navbar() {
   return (
-    <nav className='flex sticky top-0 bg-white justify-between space-x-4 max-w-screen-lg mx-auto py-2 px-6 font-semibold'>
-      <Link href={'/'}>
-        <a className='py-2 text-xl'>Kelompok 5</a>
-      </Link>
-      <ul className='flex py-2 space-x-8 items-end'>
-        {navMenu.map(([url, title], index) => (
-          <li key={index}>
-            <Link href={url}>
-              <a className='text-gray-500 hover:text-black'>{title}</a>
-            </Link>
-          </li>
-        ))}
-        <li>
+    <Popover
+      as={'nav'}
+      className='flex sticky z-50 top-0 bg-white h-16 items-center md:space-x-4 max-w-screen-lg mx-auto px-6 font-semibold'
+    >
+      <div className='flex flex-1 items-center justify-between'>
+        <div className='flex-1'>
           <Link href={'/'}>
-            <a className='rounded-md box-border border-2 border-blue-500 bg-blue-500 px-4 py-1.5 text-white hover:text-blue-500 hover:bg-white'>
+            <a className='text-xl'>Kelompok 5</a>
+          </Link>
+        </div>
+        <div className='md:flex hidden flex-1 items-center justify-around'>
+          {navMenu.map(([url, title], index) => (
+            <Link key={index} href={url}>
+              <a key={index} className='text-gray-500 hover:text-black'>
+                {title}
+              </a>
+            </Link>
+          ))}
+        </div>
+        <div className='md:flex hidden items-center space-x-6 justify-end flex-1'>
+          <Link href={'/'}>
+            <a className='rounded-md box-border border-2 border-blue-500 bg-blue-500 px-4 py-1 text-white hover:text-blue-500 hover:bg-white'>
               Source Code
             </a>
           </Link>
-        </li>
-        <li>
           <Link href={'https://www.github.com/fhrlzmn'}>
-            <a>
+            <a className=''>
               <svg
                 className='fill-gray-500 hover:fill-black'
                 height='22'
@@ -40,8 +46,52 @@ export default function Navbar() {
               </svg>
             </a>
           </Link>
-        </li>
-      </ul>
-    </nav>
+        </div>
+        {/* Button Mobile */}
+        <div className='md:hidden'>
+          <div>
+            <Popover.Button static>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M4 6h16M4 12h16M4 18h16'
+                />
+              </svg>
+            </Popover.Button>
+          </div>
+          <Transition
+            enter='transition ease-out duration-100'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='transition ease-in duration-75'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <Popover.Panel className='absolute z-50 right-0 w-full mt-4 bg-white rounded-md shadow-lg'>
+              {/* <ul> */}
+              {navMenu.map(([url, title], index) => (
+                <Link key={index} href={url}>
+                  <a
+                    key={index}
+                    className='group flex items-center w-full px-6 py-2 text-lg border-t-2'
+                  >
+                    {title}
+                  </a>
+                </Link>
+              ))}
+              {/* </ul> */}
+            </Popover.Panel>
+          </Transition>
+        </div>
+      </div>
+    </Popover>
   );
 }
